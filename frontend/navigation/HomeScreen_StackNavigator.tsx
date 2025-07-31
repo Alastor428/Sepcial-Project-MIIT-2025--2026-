c730b8a5db80d6c06edc671f8c8401df6bdb1569
Alastor428 <swanlinnhtun123@gmail.com>
2025-07-31 22:55:10 +0630

    homestack nav modefied

--- frontend/navigation/HomeScreen_StackNavigator.tsx
+++ frontend/navigation/HomeScreen_StackNavigator.tsx
@@ -1,20 +1,35 @@
+// navigation/HomeScreen_StackNavigator.tsx
+
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/main_screens/HomeScreen";
 import QRScreen from "../screens/sub_screens/Home_sub_screens/show_qr_screens/QR";
-function PlaceholderScreen({ route }) {
-  return <></>;
+import TransferScreen from "../screens/sub_screens/Home_sub_screens/transfer_screens/Transfer_1";
+
+function PlaceholderScreen() {
+  return null;
 }
 
-const Stack = createStackNavigator();
+export type RootStackParamList = {
+  HomeMain: undefined;
+  QR: undefined;
+  SetAmountScreen: undefined;
+  SaveImage: undefined;
+  Transfer: undefined;
+  CashOut: undefined;
+  CashIn: undefined;
+  QuickPay: undefined;
+  TopUp: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function HomeScreen_StackNavigator() {
   return (
     <Stack.Navigator id={undefined} screenOptions={{ headerShown: false }}>
       <Stack.Screen name="HomeMain" component={HomeScreen} />
-      {/* Register your sub-function screens here */}
       <Stack.Screen name="QR" component={QRScreen} />
-      <Stack.Screen name="Transfer" component={PlaceholderScreen} />
+      <Stack.Screen name="Transfer" component={TransferScreen} />
       <Stack.Screen name="CashOut" component={PlaceholderScreen} />
       <Stack.Screen name="CashIn" component={PlaceholderScreen} />
       <Stack.Screen name="QuickPay" component={PlaceholderScreen} />
