0d51a49e9a56b1837e7ef2e303f477c8a8833cc6
Alastor428 <swanlinnhtun123@gmail.com>
2025-08-01 14:52:59 +0630

    cancle and ok button pair, continue , done , transfer buttons created in components and app.tsx cache cleared

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,15 +3,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import { Box, NativeBaseProvider } from "native-base";
 import { theme } from "./theme";
 import { Buttonpair } from "./components/cancle_ok_buttons";
-
+import BottomTabNavigator from "./navigation/BottomTabNavigator";
 export default function App() {
   return (
     <NativeBaseProvider theme={theme}>
       <NavigationContainer>
-        <Box alignItems={"center"} marginTop={10}>
-          <Buttonpair />
-        </Box>
-        {/* <BottomTabNavigator /> */}
+        <BottomTabNavigator />
       </NavigationContainer>
     </NativeBaseProvider>
   );
