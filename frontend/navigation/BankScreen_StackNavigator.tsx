ba177d638e39ba5004678911242e379677ef5313
Kiran7981 <linn.pa.pa.khaing.2020.2021.fb@gmail.com>
2026-05-08 23:28:17 +0630

    ..

--- frontend/navigation/BankScreen_StackNavigator.tsx
+++ frontend/navigation/BankScreen_StackNavigator.tsx
@@ -4,6 +4,10 @@ import BankAccountScreen from '../screens/main_screens/Bank_Account';
 import LinkBankAccountScreen from '../screens/sub_screens/BankAccount_sub_screen/Link_BankAccount';
 import BankAccountLinkScreen from '../screens/sub_screens/BankAccount_sub_screen/Show_BankCard';
 import BankAccountPinScreen from '../screens/sub_screens/BankAccount_sub_screen/BankAccount_pinscreen';
+import CashInScreen from '../screens/sub_screens/Home_sub_screens/cash_in_screens/Cash-In';
+import CashOutScreen from '../screens/sub_screens/Home_sub_screens/cash_out_screens/cash-out-screen';
+import PinScreen from '../screens/sub_screens/Home_sub_screens/cash_out_screens/CashInOutPin_Screen';
+import TransactionDetailsScreen from '../screens/sub_screens/Home_sub_screens/transfer_screens/Transfer_5';
 
 export type BankAccountStackParamList = {
   BankAccount: { loggedInUser: any }; // receives logged-in user from login
@@ -14,6 +18,19 @@ export type BankAccountStackParamList = {
     selectedBank: string;
     bankAccount: string;
     amount?: number;
+  };
+  CashInScreen: {
+    loggedInUser: { name: string; userId: string; balance: number };
+    currentAmount?: string;
+  };
+  CashOut: { loggedInUser: any; selectedBank: string; bankAccount: string}
+  PinScreen: {
+    sender: { name: string; userId: string; pin: string };
+    recipient: { name: string; accountNumber: string };
+    amount: number;
+    bankaccount: string;
+  };
+  TransactionDetails: { transactionId: string; userId: string
   };
 };
 
@@ -27,6 +44,10 @@ export default function BankAccountStackNavigator() {
       <Stack.Screen name="LinkBankAccount" component={LinkBankAccountScreen} />
       <Stack.Screen name="BankAccountLink" component={BankAccountLinkScreen}/>
       <Stack.Screen name="BankAccountPin" component={BankAccountPinScreen}/>
+      <Stack.Screen name="CashInScreen" component={CashInScreen} />
+      <Stack.Screen name="CashOut" component={CashOutScreen} />
+      <Stack.Screen name="PinScreen" component={PinScreen} />
+      <Stack.Screen name="TransactionDetails" component={TransactionDetailsScreen} />
     </Stack.Navigator>
   );
 }
